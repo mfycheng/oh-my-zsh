9df59a656e3672233edcdf24aa31d937ec90dc5c
Arthur <mardybm@gmail.com>
2016-12-14 22:49:08 +0600

    add dotenv plugin (#4373)

--- plugins/dotenv/dotenv.plugin.zsh
+++ plugins/dotenv/dotenv.plugin.zsh
@@ -1,23 +1,10 @@
-source_env() {
-  if [[ -f $ZSH_DOTENV_FILE ]]; then
-    # test .env syntax
-    zsh -fn $ZSH_DOTENV_FILE || echo "dotenv: error when sourcing '$ZSH_DOTENV_FILE' file" >&2
+#!/bin/zsh
 
-    if [[ -o a ]]; then
-      source $ZSH_DOTENV_FILE
-    else
-      set -a
-      source $ZSH_DOTENV_FILE
-      set +a
-    fi
+source_env() {
+  if [[ -f .env ]]; then
+    source .env
   fi
 }
 
 autoload -U add-zsh-hook
 add-zsh-hook chpwd source_env
-
-if [[ -z $ZSH_DOTENV_FILE ]]; then
-    ZSH_DOTENV_FILE=.env
-fi
-
-source_env
